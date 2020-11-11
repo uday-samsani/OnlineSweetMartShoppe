5c4511049b324e66d6312104a98fdf0e0a6ae076
UdaySamsani <satyasaisamsani@gmail.com>
2020-11-11 17:41:15 +0530

    Added Admin & Added Interface UserService

--- src/main/java/com/cg/osm/entites/Admin.java
+++ src/main/java/com/cg/osm/entites/Admin.java
@@ -1,5 +1,103 @@
 package com.cg.osm.entites;
 
 public class Admin {
-	//blah blah
+	private String id;
+	private String password;
+	private Customer customer;
+	private User user;
+	private SweetItem item;
+	private Category category;
+	public Cart cart;
+	public Product product;
+	
+	// Constructor deault
+	
+	public Admin() {
+		super();
+	}
+	
+	// Constructor with fields
+	public Admin(String id, String password, Customer customer, User user, SweetItem item, Category category, Cart cart,
+			Product product) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.customer = customer;
+		this.user = user;
+		this.item = item;
+		this.category = category;
+		this.cart = cart;
+		this.product = product;
+	}
+	
+	// Getters
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public User getUser() {
+		return user;
+	}
+	
+	// Setters
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public SweetItem getItem() {
+		return item;
+	}
+
+	public void setItem(SweetItem item) {
+		this.item = item;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public Cart getCart() {
+		return cart;
+	}
+
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	
+	
 }
--- src/main/java/com/cg/osm/service/UserService.java
+++ src/main/java/com/cg/osm/service/UserService.java
@@ -1,5 +1,8 @@
 package com.cg.osm.service;
 
 public interface UserService {
-
+	public User addUser(User user);
+	public User updateUser(User user);
+	public User cancelUser(User user);
+	public User showAllUsers(User user);
 }
