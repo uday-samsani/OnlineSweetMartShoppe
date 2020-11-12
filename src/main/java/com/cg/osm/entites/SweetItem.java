43a41a5a60001c8836c05ddde815e3ba6e3d442e
nikhinikhil <nikhila23.1998@gmail.com>
2020-11-12 09:16:36 +0530

    Added SweetItem Entity

--- src/main/java/com/cg/osm/entites/SweetItem.java
+++ src/main/java/com/cg/osm/entites/SweetItem.java
@@ -1,5 +1,43 @@
 package com.cg.osm.entites;
 
-public class SweetItem {
+public class SweetItem 
+{
+    private int orderItemId;
+    private Product product;
+    private SweetOrder SweetOrder;
+    
+    
+    public SweetItem() {
+    	super();
+    }
+	public SweetItem(int orderItemId, Product product, com.cg.osm.entites.SweetOrder sweetOrder) 
+	{
+		super();
+		this.orderItemId = orderItemId;
+		this.product = product;
+		SweetOrder = sweetOrder;
+	}
+	
+	//Getters and Setters
+	public int getOrderItemId() {
+		return orderItemId;
+	}
+	public void setOrderItemId(int orderItemId) {
+		this.orderItemId = orderItemId;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public SweetOrder getSweetOrder() {
+		return SweetOrder;
+	}
+	public void setSweetOrder(SweetOrder sweetOrder) {
+		SweetOrder = sweetOrder;
+	}
 
+	
+    
 }
--- src/main/java/com/cg/osm/repository/SweetItemRepository.java
+++ src/main/java/com/cg/osm/repository/SweetItemRepository.java
@@ -1,5 +1,16 @@
 package com.cg.osm.repository;
 
+import java.util.List;
+
+import com.cg.osm.entites.SweetItem;
+
 public interface SweetItemRepository {
 
+	public abstract void addSweetItem(SweetItem sweetItem);
+	
+	public abstract void updateSweetItem(SweetItem sweetItem);
+	
+	public abstract void cancelSweetItem(int sweetItem);
+	
+	public abstract List<SweetItem> showAllSweetItems();
 }
